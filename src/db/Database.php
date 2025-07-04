48f03b9a77871cf777f3e97715abf620ddc0ff62
Neon <tpshine1234@gmail.com>
2025-07-05 03:21:33 +0630

    fix: removed exposing line of db_uri to client

--- src/db/Database.php
+++ src/db/Database.php
@@ -39,8 +39,7 @@ class Database
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ];
-
-        echo "Connecting to database: $dsn\n";
+        
         self::$pdo = new PDO($dsn, $user, $pass, $options);
     }
 
